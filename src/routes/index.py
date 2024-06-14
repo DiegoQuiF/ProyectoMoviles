a1736d4cdd855cfa83129bc08871ad377a331083
DiegoQuiF <diego.quinonez@unmsm.edu.pe>
2024-06-14 10:39:39 -0500

    v1.0.0

--- src/routes/index.py
+++ src/routes/index.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, jsonify, request
 
-from src.services.post.postModel import postModel
+from src.services.post.postLogin import postLogin
+from src.services.post.postRegister import postRegister
+
+from src.models.Paciente import Paciente
 
 
 main = Blueprint('index_blueprint', __name__)
@@ -9,12 +12,28 @@ main = Blueprint('index_blueprint', __name__)
 def iniciarSesion():
   try:
     data = request.get_json()
-    email = data['email_usu']
-    contra = data['contra_usu']
-    id_usu = postModel(email, contra)
-    if(id_usu!=""):
-      datos = {'id_usu':id_usu,}
-      return jsonify({'message':'COMPLETE', 'success':True, 'data':datos})
+    email = data['email']
+    contra = data['contra']
+    paciente = postLogin(email, contra)
+    if(paciente!=''):
+      paciente = paciente.to_json()
+      return jsonify({'message':'COMPLETE', 'success':True, 'data':paciente})
+    else:
+      return jsonify({'message':'NOT FOUND', 'success':True})
+  except Exception as e:
+    return jsonify({'message':'ERROR', 'success':False})
+
+@main.route("/registrar", methods = ['POST'])
+def register():
+  try:
+    data = request.get_json()
+    nom_comp = data['nom_comp']
+    direc = data['direc']
+    email = data['email']
+    contra = data['contra']
+    registrado = postRegister(nom_comp, direc, email, contra)
+    if(registrado):
+      return jsonify({'message':'COMPLETE', 'success':True})
     else:
       return jsonify({'message':'NOT FOUND', 'success':True})
   except Exception as e:
